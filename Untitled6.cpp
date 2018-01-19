64ae5c7c9b0e9b2eb85ea0b002c05c4a824817ff
adriancahayaputra <adriancahayaputra@gmail.com>
2018-01-20 00:20:02 +0800

    terjamin baik

--- Untitled6.cpp
+++ Untitled6.cpp
@@ -8,3 +8,17 @@ class kaca{
 		string merk;
 		double harga;
 };
+int main () {
+	kaca plastik;
+	
+	plastik.merk ="kaca";
+	plastik.harga = 10000;
+	
+	cout<<"merk yang dipakai "<< (plastik.merk)<<endl;
+	cout<<"Harga modal nya "<< (plastik.harga)<<endl;
+	
+	plastik.bahan("terbuat dari plastik yang berkualitas dijamin tahan lama");
+}
+void kaca::bahan(string plastik){
+	cout<<"lebih tahan lama dari pada kaca yang lainnya"<<plastik<<"harga ekonomis dijamin!"<<endl;
+}
